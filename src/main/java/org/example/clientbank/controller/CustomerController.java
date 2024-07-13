c6d746ee0fb3644cfbffae93c31629c7467290f8
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-13 17:42:40 +0300

    done

--- src/main/java/org/example/clientbank/controller/CustomerController.java
+++ src/main/java/org/example/clientbank/controller/CustomerController.java
@@ -109,6 +109,7 @@ public class CustomerController {
         }
     }
 
+
     @PostMapping("/create_account_by_id")
     public ResponseEntity<String> createAccountByCustomerId(@RequestBody CreateAccountByIdModel createAccountByIdModel) {
         log.info("Trying to create account by customer id");
