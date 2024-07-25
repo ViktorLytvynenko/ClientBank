1015abad67f7583d164d53bf18a745ff3f4a3e48
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-26 01:18:40 +0300

    done

--- src/main/java/org/example/clientbank/AbstractEntity.java
+++ src/main/java/org/example/clientbank/AbstractEntity.java
@@ -2,32 +2,29 @@ package org.example.clientbank;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.experimental.FieldDefaults;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 import static jakarta.persistence.TemporalType.TIMESTAMP;
-import static lombok.AccessLevel.PUBLIC;
 
 @MappedSuperclass
-@FieldDefaults(level = PUBLIC)
 @Data
 public abstract class AbstractEntity {
 
     @Id
     @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
     @CreatedDate
     @Temporal(TIMESTAMP)
     @Column(updatable = false, nullable = false)
-    LocalDateTime createdDate;
+    protected LocalDateTime createdDate;
 
     @LastModifiedDate
     @Temporal(TIMESTAMP)
     @Column(nullable = false)
-    LocalDateTime lastModifiedDate;
+    protected LocalDateTime lastModifiedDate;
 }
