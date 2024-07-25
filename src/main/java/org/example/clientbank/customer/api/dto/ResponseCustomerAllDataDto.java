8280ad8ba135f2507c434e887d221bd18432c213
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-26 01:39:29 +0300

    done

--- src/main/java/org/example/clientbank/customer/api/dto/ResponseCustomerAllDataDto.java
+++ src/main/java/org/example/clientbank/customer/api/dto/ResponseCustomerAllDataDto.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
-import org.example.clientbank.account.Account;
-import org.example.clientbank.employer.Employer;
+import org.example.clientbank.account.api.dto.ResponseAccountDto;
+import org.example.clientbank.employer.api.dto.ResponseEmployerDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,6 +25,6 @@ public class ResponseCustomerAllDataDto {
     String phone;
     LocalDateTime createdDate;
     LocalDateTime lastModifiedDate;
-    List<Account> accounts;
-    List<Employer> employers;
+    List<ResponseAccountDto> accounts;
+    List<ResponseEmployerDto> employers;
 }
