b08f64db2ef7eaeb6daaee6bc0e7171bf06ce91f
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-26 00:58:36 +0300

    done

--- src/main/resources/sql/schema-postgres.sql
+++ src/main/resources/sql/schema-postgres.sql
@@ -8,9 +8,9 @@ DROP TABLE IF EXISTS employers;
 create table customers
 (
     id                 SERIAL PRIMARY KEY,
-    age                integer                             not null,
-    email              varchar(255)                        not null unique,
-    name               varchar(255)                        not null,
+    age                integer                             NOT NULL,
+    email              varchar(255)                        NOT NULL UNIQUE,
+    name               varchar(255)                        NOT NULL,
     password           VARCHAR(255)                        NOT NULL,
     phone              VARCHAR(20)                         NOT NULL,
     created_date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -20,9 +20,9 @@ create table customers
 create table accounts
 (
     id                 SERIAL PRIMARY KEY,
-    balance            float(53)                           not null,
-    currency           varchar(3)                          not null check (currency in ('USD', 'EUR', 'UAH', 'CHF', 'GBP')),
-    number             varchar(255)                        not null unique,
+    balance            float(53)                           NOT NULL,
+    currency           varchar(3)                          NOT NULL CHECK (currency in ('USD', 'EUR', 'UAH', 'CHF', 'GBP')),
+    number             varchar(255)                        NOT NULL UNIQUE,
     customer_id        bigint,
     created_date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -32,8 +32,8 @@ create table accounts
 create table employers
 (
     id                 SERIAL PRIMARY KEY,
-    address            varchar(255)                        not null,
-    name               varchar(255)                        not null,
+    address            varchar(255)                        NOT NULL,
+    name               varchar(255)                        NOT NULL,
     created_date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     last_modified_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
