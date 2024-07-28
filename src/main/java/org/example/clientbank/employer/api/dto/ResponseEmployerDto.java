f1e219272146103c0290aabc2f76d25543b63a8c
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-07-28 20:58:24 +0300

    done

--- src/main/java/org/example/clientbank/employer/api/dto/ResponseEmployerDto.java
+++ src/main/java/org/example/clientbank/employer/api/dto/ResponseEmployerDto.java
@@ -1,9 +1,11 @@
 package org.example.clientbank.employer.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.FieldDefaults;
+import org.example.clientbank.customer.api.dto.View;
 
 import static lombok.AccessLevel.PRIVATE;
 
@@ -13,7 +15,12 @@ import static lombok.AccessLevel.PRIVATE;
 @AllArgsConstructor
 @Data
 public class ResponseEmployerDto {
+    @JsonView(View.Admin.class)
     Long id;
+
+    @JsonView(View.Admin.class)
     String name;
+
+    @JsonView(View.Admin.class)
     String address;
 }
