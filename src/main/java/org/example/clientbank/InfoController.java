37302f3ed546782317cee34ec01d80f02f447e05
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-08-07 19:09:04 +0300

    added thymeleaf

--- src/main/java/org/example/clientbank/InfoController.java
+++ src/main/java/org/example/clientbank/InfoController.java
@@ -1,13 +1,15 @@
 package org.example.clientbank;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class InfoController {
 
-    public String hello() {
-        return "forward:/index.html";
+    @GetMapping
+    public String start() {
+        return "index";
     }
 }
