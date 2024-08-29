7b2b6322506a2e1e8f79107292b151e55a744d59
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-08-29 18:06:08 +0300

    some fixes

--- src/main/java/org/example/clientbank/security/SysUser/db/UserRepository.java
+++ src/main/java/org/example/clientbank/security/SysUser/db/UserRepository.java
@@ -3,10 +3,12 @@ package org.example.clientbank.security.SysUser.db;
 import org.example.clientbank.security.SysUser.SysUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<SysUser, Long> {
 
     @Query("SELECT u FROM SysUser u WHERE u.userName = :username")
